7596a690c16bf8fc572e612ab32531ad99a91d90
Sergey Kozlov <zeemoney@yandex.ru>
2022-06-21 18:39:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,4 @@ import './style/globals.scss'
 import { App } from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-)
+root.render(<App />)
